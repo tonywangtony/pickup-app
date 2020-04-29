b38758eeb4fced6d6bdcd2932abef25c5f0e4209
goodjun <goodjun@163.com>
2020-04-28 19:45:10 -0700

    update

--- src/pages/Assgin/Assgin.vue
+++ src/pages/Assgin/Assgin.vue
@@ -5,20 +5,26 @@
         small
         color="primary"
         class="ml-2"
-        @click="staffSheet = true"
+        @click="lock()"
         :disabled="activeTab !== 1"
+        >Lock</v-btn
       >
-        <v-icon left small>fas fa-long-arrow-alt-right</v-icon> Assign
-      </v-btn>
       <v-btn
         small
         color="primary"
         class="ml-2"
-        @click="unassign"
+        @click="unlock()"
         :disabled="activeTab !== 2"
+        >Unlock</v-btn
       >
-        <v-icon left small>fas fa-long-arrow-alt-right</v-icon> Unassign
-      </v-btn>
+      <v-btn
+        small
+        color="primary"
+        class="ml-2"
+        @click="done()"
+        :disabled="activeTab !== 2"
+        >Done</v-btn
+      >
     </v-row>
     <v-row>
       <v-col>
@@ -28,13 +34,14 @@
         <v-select :items="types2" label="Type 2"></v-select>
       </v-col>
     </v-row>
+    <!-- tabs -->
     <v-row align="center">
       <v-col>
         <v-tabs class="mt-1" dense>
-          <v-tab @click="switchTab(1)">Pending({{ pendingCount }})</v-tab>
-          <v-tab @click="switchTab(2)">Assigned({{ assignedCount }})</v-tab>
-          <v-tab @click="switchTab(3)">Completed({{ completedCount }})</v-tab>
-          <v-tab @click="switchTab(4)">All({{ allCount }})</v-tab>
+          <v-tab @click="switchTab(1)"> Pool({{ pool.length }}) </v-tab>
+          <v-tab @click="switchTab(2)">Locked({{ locked.length }})</v-tab>
+          <v-tab @click="switchTab(3)">Completed({{ completed.length }})</v-tab>
+          <v-tab @click="switchTab(4)">All({{ all.length }})</v-tab>
         </v-tabs>
       </v-col>
       <v-col>
@@ -49,84 +56,129 @@
       </v-col>
     </v-row>
     <!-- tables -->
-    <Pending
-      ref="pending"
-      v-if="activeTab == 1"
-      v-model="search"
-      @onShowPo="onShowPo"
-    ></Pending>
-    <Assigned ref="assigned" v-if="activeTab == 2" v-model="search"></Assigned>
-    <Completed v-if="activeTab == 3" v-model="search"></Completed>
-    <All v-if="activeTab == 4" v-model="search"></All>
-    <!-- po sheet -->
-    <v-bottom-sheet v-model="poSheet">
-      <v-sheet class="text-center" height="100px">
-        <div class="py-3">
-          <p>PO: 5000000001</p>
-          <p>ON-hand: 521.00</p>
-        </div>
-      </v-sheet>
-    </v-bottom-sheet>
-    <!-- staff sheet -->
-    <v-bottom-sheet v-model="staffSheet">
-      <v-sheet class="text-center" height="600px">
-        <v-row justify="start" class="ml-2 pt-2">
-          <v-col sm="10">
-            <v-select :items="staffs" value="TOM.L" solo></v-select>
-          </v-col>
-          <v-col sm="2">
-            <v-btn color="success" @click="assign">
-              Done
-            </v-btn>
-          </v-col>
-        </v-row>
-      </v-sheet>
-    </v-bottom-sheet>
+    <v-data-table
+      :headers="headers"
+      :items="productsFilter"
+      :search="search"
+      hide-default-footer
+    >
+      <template v-slot:body="{ items }">
+        <tbody>
+          <tr v-for="item in items" :key="item.id">
+            <td>
+              <v-checkbox v-model="selected" :value="item.id"></v-checkbox>
+            </td>
+            <td>{{ item.index }}</td>
+            <td>{{ item.id }}</td>
+            <td>{{ item.name }}</td>
+            <td>{{ item.um }}</td>
+            <td>{{ item.subtotal }}</td>
+            <td>{{ item.assignee }}</td>
+          </tr>
+        </tbody>
+      </template>
+    </v-data-table>
   </v-container>
 </template>
 
 <script>
-import Pending from "./Pending";
-import Assigned from "./Assigned";
-import Completed from "./Completed";
-import All from "./All";
-
 export default {
   name: "Assgin",
-  components: {
-    Pending,
-    Assigned,
-    Completed,
-    All,
-  },
+  components: {},
   data() {
     return {
       activeTab: 1,
-      staffSheet: false,
-      poSheet: false,
       search: "",
-      staffs: ["TOM.L"],
+      selected: [],
+      headers: [
+        { text: "", value: "null", sortable: false },
+        { text: "Index", value: "index" },
+        { text: "Item", value: "id" },
+        { text: "Description", value: "name" },
+        { text: "U/M", value: "um" },
+        { text: "Subtotal", value: "subtotal" },
+      ],
+      products: [
+        {
+          index: 1,
+          id: "10010385",
+          name: "Game Hen·Patti·0824 <24oz>·24HD·白皮童子雞",
+          um: "CS",
+          subtotal: 6,
+          status: 0,
+        },
+        {
+          index: 2,
+          id: "10010965",
+          name: "Whole Young Chix·PG·112939·CW·細雞仔",
+          um: "CS",
+          subtotal: 13,
+          status: 0,
+        },
+        {
+          index: 3,
+          id: "10010026PC",
+          name: "Chix Bone Frames·Pitman·60019·#40·雞胸骨",
+          um: "PC",
+          subtotal: 2,
+          status: 0,
+        },
+        {
+          index: 4,
+          id: "10010114PC",
+          name: "PC-Breast Mt·Mountaire·25630·#10·雞胸肉",
+          um: "PC",
+          subtotal: 12,
+          status: 0,
+        },
+        {
+          index: 5,
+          id: "10011295",
+          name: "Thighs (B/In)·Perdue·623·#40·帶骨上脾",
+          um: "PC",
+          subtotal: 17,
+          status: 0,
+        },
+        {
+          index: 6,
+          id: "10011595PC",
+          name: "PC-Thighs (B/In)·S&J··(#4.5-5)·帶骨上脾",
+          um: "PC",
+          subtotal: 15,
+          status: 0,
+        },
+      ],
     };
   },
   methods: {
     switchTab(id) {
       this.activeTab = id;
       this.search = "";
+      this.selected = [];
     },
-    onShowPo(data) {
-      console.log(data);
-      this.poSheet = true;
+    lock() {
+      this.products.map((product) => {
+        if (this.selected.includes(product.id)) {
+          product.status = 1;
+        }
+        return product;
+      });
     },
-    assign() {
-      this.staffSheet = false;
-      if (this.$store.state.user.ing) {
-        alert("TOM.L 不能接收分配");
-      } else {
-        this.$refs.pending.assign("TOM.L");
-      }
+    unlock() {
+      this.products.map((product) => {
+        if (this.selected.includes(product.id)) {
+          product.status = 0;
+        }
+        return product;
+      });
     },
-    unassign() {
-      this.$refs.assigned.unassign();
+    done() {
+      this.products.map((product) => {
+        if (this.selected.includes(product.id)) {
+          product.status = 2;
+        }
+        return product;
+      });
     },
   },
   computed: {
@@ -136,23 +188,31 @@ export default {
     types2() {
       return this.$store.state.productType.types2;
     },
-    pendingCount() {
-      return this.$store.state.products.all.filter(
-        (product) => product.assignee == ""
-      ).length;
+    pool() {
+      return this.products.filter((item) => item.status === 0);
     },
-    assignedCount() {
-      return this.$store.state.products.all.filter(
-        (product) => product.status === 1 || product.status === 2
-      ).length;
+    locked() {
+      return this.products.filter((item) => item.status === 1);
     },
-    completedCount() {
-      return this.$store.state.products.all.filter(
-        (product) => product.status === 3
-      ).length;
+    completed() {
+      return this.products.filter((item) => item.status === 2);
     },
-    allCount() {
-      return this.$store.state.products.all.length;
+    all() {
+      return this.products;
+    },
+    productsFilter() {
+      switch (this.activeTab) {
+        case 1:
+          return this.products.filter((item) => item.status === 0);
+        case 2:
+          return this.products.filter((item) => item.status === 1);
+        case 3:
+          return this.products.filter((item) => item.status === 2);
+        case 4:
+          return this.products;
+        default:
+          return [];
+      }
     },
   },
 };
